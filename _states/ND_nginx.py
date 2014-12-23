7c1520e419dd21a7e3080bbb33562cee0bf40540
fuqiang89 <wfuqiang89@gmail.com>
2014-12-23 16:49:07 +0800

    add new nd_nginx module

---
+++ _states/ND_nginx.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Import python libs
+import sys
+
+# Import salt libs
+from salt.exceptions import CommandExecutionError
+from salt.modules.aptpkg import _strip_uri
+from salt.state import STATE_INTERNAL_KEYWORDS as _STATE_INTERNAL_KEYWORDS
+
+
+def __virtual__():
+    '''
+    Only load if modifying repos is available for this package type
+    '''
+    return 'pkg.mod_repo' in __salt__
+
+
+def _error(ret, err_msg):
+    ret['result'] = False
+    ret['comment'] = err_msg
+    return ret
+
+def installed(name,
+            source=None,
+            source_hash='',
+            add-module=None,
+
+            **kwargs)
