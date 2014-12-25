89087b91264e56f195a7523c52524ef75393f641
fuqiang89 <wfuqiang89@gmail.com>
2014-12-25 18:11:21 +0800

    add test state

--- _module/nd_nginx.py
+++ _module/nd_nginx.py
@@ -1,24 +1,72 @@
 # -*- coding: utf-8 -*-
 
+# TODO: We should add the capability to do u+r type operations here
+# some time in the future
+
+from __future__ import print_function
+
 # Import python libs
+import contextlib  # For < 2.7 compat
+import datetime
+import difflib
+import errno
+import fileinput
+import fnmatch
+import itertools
+import logging
+import operator
+import os
+import re
+import shutil
+import stat
 import sys
+import tempfile
+import time
+import glob
+
+try:
+    import grp
+    import pwd
+except ImportError:
+    pass
 
 # Import salt libs
-from salt.exceptions import CommandExecutionError
-from salt.modules.aptpkg import _strip_uri
-from salt.state import STATE_INTERNAL_KEYWORDS as _STATE_INTERNAL_KEYWORDS
+import salt.utils
+import salt.utils.find
+import salt.utils.filebuffer
+import salt.utils.files
+import salt.utils.atomicfile
+from salt.exceptions import CommandExecutionError, SaltInvocationError
+import salt._compat
+
+log = logging.getLogger(__name__)
 
 
 def __virtual__():
     '''
-    Only load if modifying repos is available for this package type
+    Only work on POSIX-like systems
     '''
-    return 'pkg.mod_repo' in __salt__
-
+    # win_file takes care of windows
+    if salt.utils.is_windows():
+        return False
+    return True
 
 def _error(ret, err_msg):
+    '''
+    Common function for setting error information for return dicts
+    '''
     ret['result'] = False
     ret['comment'] = err_msg
     return ret
+
+def test():
+    ret ={}
+    try:
+        f=open('log.txt','a')
+        f.write()
+    except Exception,e:
+        ret['result'] = False
+        ret['comment'] = e
+        return ret
 
 
--- _states/ND_nginx.py
+++ _states/ND_nginx.py
@@ -7,14 +7,26 @@ import sys
 from salt.exceptions import CommandExecutionError
 from salt.modules.aptpkg import _strip_uri
 from salt.state import STATE_INTERNAL_KEYWORDS as _STATE_INTERNAL_KEYWORDS
+# __virtualname__ = 'nd_nginx'
+#
+# def __virtual__():
+#
+#     return __virtualname__
 
 
-def __virtual__():
-    '''
-    Only load if modifying repos is available for this package type
-    '''
-    return 'pkg.mod_repo' in __salt__
 
+'''
+configure arguments:
+--prefix=/usr/local/nginx-1.6.2
+--with-http_flv_module
+--with-http_mp4_module
+--with-http_stub_status_module
+--with-http_ssl_module
+--with-pcre=/usr/local/src/nginx_install/pcre-8.36
+--with-zlib=/usr/local/src/nginx_install/zlib-1.2.8
+--with-openssl=/usr/local/src/nginx_install/openssl-1.0.1j
+--add-module=
+'''
 
 def _error(ret, err_msg):
     ret['result'] = False
@@ -24,6 +36,12 @@ def _error(ret, err_msg):
 def installed(name,
             source=None,
             source_hash='',
-            add-module=None,
-
-            **kwargs)
+            add_module=None,
+            configure=None,
+            prefix=None,
+            **kwargs):
+    ret={}
+    try:
+        __salt__['nd_nginx.test'](source)
+    except Exception,e:
+        return _error(ret,e)
